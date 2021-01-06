209c4e036d35914af6f81207a5425f54e70a648c
yonatankra <yonatan.kra@vonage.com>
2021-01-06 16:36:26 +0200

    Add localization and the header component

---
+++ apps/user-interface/src/app/app-header/app-header.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vacgaps-app-header',
+  templateUrl: './app-header.component.html',
+  styleUrls: ['./app-header.component.scss']
+})
+export class AppHeaderComponent implements OnInit {
+  @Input()
+  title: string;
+
+  constructor() {
+    this.title = 'Test';
+  }
+
+  ngOnInit(): void {
+  }
+
+}
--- apps/user-interface/src/app/app.module.ts
+++ apps/user-interface/src/app/app.module.ts
@@ -5,12 +5,23 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { AppHeaderComponent } from './app-header/app-header.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    AppHeaderComponent,
+  ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
