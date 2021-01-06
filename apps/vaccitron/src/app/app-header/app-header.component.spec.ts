ee2720d1481af4c95b9b00f96e9e1da3ddc77012
yonatankra <yonatan.kra@vonage.com>
2021-01-06 17:10:52 +0200

    Setup vaccitron to be used with Karma

---
+++ apps/vaccitron/src/app/app-header/app-header.component.spec.ts
@@ -0,0 +1,53 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppHeaderComponent } from './app-header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { By } from '@angular/platform-browser';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-component',
+  template: `<vacgaps-app-header [title]="title"></vacgaps-app-header>`,
+})
+export class TestComponent {
+  title = 'user-interface';
+}
+describe('AppHeaderComponent', () => {
+  let component: AppHeaderComponent;
+  let fixture: ComponentFixture<AppHeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+      declarations: [ AppHeaderComponent, TestComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppHeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(`should display the site's title`, function() {
+    const title = 'This is a title';
+    component.title = title;
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('h1')).nativeElement.textContent).toEqual(title);
+  });
+
+  it(`should get the title as an input`, function() {
+    const appTitle = 'This is a title from parent';
+    const parentFixture = TestBed.createComponent(TestComponent);
+    parentFixture.componentInstance.title = appTitle;
+    parentFixture.detectChanges();
+    expect(parentFixture.debugElement.query(By.css('vacgaps-app-header h1')).nativeElement.textContent).toEqual(appTitle);
+  });
+});
---
+++ apps/vaccitron/src/app/app.component.spec.ts
@@ -0,0 +1,21 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent, AppHeaderComponent],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+});
