7bc58b93e2b26fabbc99fb1c11ae09fc931bd206
yonatankra <yonatan.kra@vonage.com>
2021-01-06 17:19:45 +0200

    Add app's title

--- apps/vaccitron/src/app/app.component.spec.ts
+++ apps/vaccitron/src/app/app.component.spec.ts
@@ -5,17 +5,23 @@ import { AppHeaderComponent } from './app-header/app-header.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
+  let fixture;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [AppComponent, AppHeaderComponent],
       schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it(`should have app name as Friends to Vaccines`, function() {
+    expect(fixture.componentInstance.name).toEqual( "Friends to Vaccines");
   });
 });
