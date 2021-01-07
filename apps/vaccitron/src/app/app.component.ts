474953d1aa9a8c3ac3dc917623020cf759cc96bc
yonatankra <yonatan.kra@vonage.com>
2021-01-07 21:04:24 +0200

    Use the cities constant

--- apps/vaccitron/src/app/app.component.ts
+++ apps/vaccitron/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserDetails } from './user-details/form/form.component';
+import { CITIES } from '@vacgaps/constants';
 
 @Component({
   selector: 'vacgaps-root',
@@ -8,6 +9,7 @@ import { UserDetails } from './user-details/form/form.component';
 })
 export class AppComponent {
   name = "Friends to Vaccines";
+  cities = new Map(Object.entries(CITIES));
 
   handleUserDetails(data: UserDetails) {
   }
