5ae57b1b7848bf643befa6485fe27e620332d600
gilm123 <gilm123@gmail.com>
2021-01-09 15:41:04 +0200

    Revert "Merge branch 'main' of https://github.com/YonatanKra/vacgaps into add-facebook-login-playground-scripts"
    This reverts commit acd62d6449e7a18da39a928bb6cebdc2464d5c18.

--- backend/azure-functions/deploy.sh
+++
@@ -1,7 +0,0 @@
-cd dist
-zip -r dist.zip *
-# get secret from downloading the publish profile from:
-# https://portal.azure.com/#@idanigrahotmail.onmicrosoft.com/resource/subscriptions/8d9dd7e5-2464-4c47-a649-7676fc13da66/resourcegroups/vacgaps/providers/Microsoft.Web/sites/vacgaps/appServices
-# copy the value named "userPWD"
-export secret=
-curl -X POST -u \$vacgaps:$secret --data-binary @dist.zip https://vacgaps.scm.azurewebsites.net/api/zipdeploy
