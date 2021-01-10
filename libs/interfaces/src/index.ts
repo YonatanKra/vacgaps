a06623973eaad12a436812161c15b9f5a5e85063
yonatankra <yonatan.kra@vonage.com>
2021-01-10 08:42:06 +0200

    Set the first interfaces
    Filter and report

--- libs/interfaces/src/index.ts
+++ libs/interfaces/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/notifications-filter';
+export * from './lib/vaccines-report';
--- libs/interfaces/src/lib/vaccines-report.ts
+++ libs/interfaces/src/lib/vaccines-report.ts
@@ -1,9 +1,9 @@
-import { CITIES_TYPE } from '@vacgaps/constants';
+import { CITIES_TYPE, HEALTH_CARE_SERVICES_TYPE } from '@vacgaps/constants';
 
 export interface VaccinesReport {
   city: CITIES_TYPE[number];
   address: string;
-  healthCareService: number;
+  healthCareService: HEALTH_CARE_SERVICES_TYPE[number];
   availableVaccines?: number;
   dueTimeInMs?: number;
   targetGroup?: number;
