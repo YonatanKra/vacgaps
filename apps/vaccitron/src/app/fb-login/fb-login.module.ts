a878a5c1baea634963fffe61f993c375c1e719fd
Idan Igra <idigra@microsoft.com>
2021-01-11 12:50:21 +0200

    Follow angularx-social-login tutorial

--- apps/vaccitron/src/app/fb-login/fb-login.module.ts
+++ apps/vaccitron/src/app/fb-login/fb-login.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FbLoginComponent } from './fb-login/fb-login.component';
+import { SocialLoginModule, AuthServiceConfig, FacebookLoginProvider } from 'angularx-social-login';
 
+const config = new AuthServiceConfig([
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider('416321649791285')
+  }
+]);
 
+export function provideConfig() {
+  return config;
+}
 
 @NgModule({
   declarations: [FbLoginComponent],
   exports: [FbLoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SocialLoginModule
+  ],
+  providers: [
+    {
+      provide: AuthServiceConfig,
+      useFactory: provideConfig
+    }
   ]
 })
 export class FbLoginModule { }
