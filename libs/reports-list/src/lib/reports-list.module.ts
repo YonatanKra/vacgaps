8c1001211e2ee1a7c7f10c80862d91d665924fd1
yonatankra <yonatan.kra@vonage.com>
2021-01-11 16:15:09 +0200

    Add the report list component

--- libs/reports-list/src/lib/reports-list.module.ts
+++ libs/reports-list/src/lib/reports-list.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReportsListComponent } from './reports-list/reports-list.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ReportsListComponent],
 })
 export class ReportsListModule {}
---
+++ libs/reports-list/src/lib/reports-list/reports-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vacgaps-reports-list',
+  templateUrl: './reports-list.component.html',
+  styleUrls: ['./reports-list.component.scss']
+})
+export class ReportsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
