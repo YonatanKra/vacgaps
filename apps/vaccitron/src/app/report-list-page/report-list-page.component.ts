6a663bda6c4ed0854578cb0d798d103646e91c96
yonatankra <yonatan.kra@vonage.com>
2021-01-12 08:47:46 +0200

    Mock data

--- apps/vaccitron/src/app/report-list-page/report-list-page.component.ts
+++ apps/vaccitron/src/app/report-list-page/report-list-page.component.ts
@@ -8,7 +8,33 @@ import { NotificationsFilter, VaccinesReport } from '@vacgaps/interfaces';
 })
 export class ReportListPageComponent implements OnInit {
   @Input()
-  reportsList: VaccinesReport[];
+  reportsList: VaccinesReport[] = [
+    {
+      city: '10',
+      healthCareService: '1',
+      address: 'שרה אמנו 39'
+    },
+    {
+      city: '1001',
+      healthCareService: '2',
+      address: 'שרה אמנו 39'
+    },
+    {
+      city: '530',
+      healthCareService: '0',
+      address: 'שרה אמנו 39'
+    },
+    {
+      city: '2379',
+      healthCareService: '3',
+      address: 'שרה אמנו 39'
+    },
+    {
+      city: '110',
+      healthCareService: '1',
+      address: 'שרה אמנו 39'
+    }
+  ];
 
   get filteredReportsList(): VaccinesReport[] {
     return this.filterList(this.#currentFilter);
