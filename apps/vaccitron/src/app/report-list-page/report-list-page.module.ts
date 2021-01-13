a663c4e3b6657cd2d69ecd783dc22e4d87151f77
Yonatan Kra <yonatan.kra@vonage.com>
2021-01-13 15:34:40 +0200

    Poll from server (#39)

--- apps/vaccitron/src/app/report-list-page/report-list-page.module.ts
+++ apps/vaccitron/src/app/report-list-page/report-list-page.module.ts
@@ -4,13 +4,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { ReportListPageComponent } from './report-list-page.component';
 import { FilterFormModule } from '@vacgaps/filter-form';
 import { ReportsListModule } from '@vacgaps/reports-list';
+import { VaccinesReportsService } from '@vacgaps/vaccines-reporter';
+import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [{ path: '', component: ReportListPageComponent }];
 
 @NgModule({
+  providers: [VaccinesReportsService],
   declarations: [ReportListPageComponent],
   imports: [
     CommonModule,
+    HttpClientModule,
     RouterModule.forChild(routes),
     FilterFormModule,
     ReportsListModule,
