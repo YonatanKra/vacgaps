ddfd1878ac3cb3123eb4a35393c1fe60fff629d4
yonatankra <yonatan.kra@vonage.com>
2021-01-14 08:54:08 +0200

    Make tests pass

--- apps/vaccitron/src/app/fb-login/fb-login/fb-login.component.spec.ts
+++ apps/vaccitron/src/app/fb-login/fb-login/fb-login.component.spec.ts
@@ -1,6 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FbLoginComponent } from './fb-login.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { SocialAuthService, SocialUser } from 'angularx-social-login';
+import { Observable } from 'rxjs';
+
+const MockSocialAuthService = {
+  _authState: undefined,
+  _initState: undefined,
+  _user: undefined,
+  autoLogin: undefined,
+  initialize: undefined,
+  initialized: undefined,
+  providers: undefined,
+  get authState(): Observable<SocialUser> {
+    return new Observable<SocialUser>();
+  },
+  get initState(): Observable<boolean> {
+    return new Observable<boolean>();
+  },
+  refreshAuthToken(providerId: string): Promise<void> {
+    return Promise.resolve(undefined);
+  },
+  signIn(providerId: string, signInOptions?: any): Promise<SocialUser> {
+    return Promise.resolve(undefined);
+  },
+  signOut(revoke?: boolean): Promise<any> {
+    return Promise.resolve(undefined);
+  }
+};
 
 describe('FbLoginComponent', () => {
   let component: FbLoginComponent;
@@ -8,7 +36,9 @@ describe('FbLoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FbLoginComponent ]
+      providers: [{provide: SocialAuthService, useValue: MockSocialAuthService}],
+      declarations: [ FbLoginComponent ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   });
--- apps/vaccitron/src/app/guards/auth.guard.spec.ts
+++ apps/vaccitron/src/app/guards/auth.guard.spec.ts
@@ -1,12 +1,47 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthGuard } from './auth.guard';
+import { Observable } from 'rxjs';
+import { SocialAuthService, SocialUser } from 'angularx-social-login';
+import { Router, UrlTree } from '@angular/router';
 
+const MockSocialAuthService = {
+  _authState: undefined,
+  _initState: undefined,
+  _user: undefined,
+  autoLogin: undefined,
+  initialize: undefined,
+  initialized: undefined,
+  providers: undefined,
+  get authState(): Observable<SocialUser> {
+    return new Observable<SocialUser>();
+  },
+  get initState(): Observable<boolean> {
+    return new Observable<boolean>();
+  },
+  refreshAuthToken(providerId: string): Promise<void> {
+    return Promise.resolve(undefined);
+  },
+  signIn(providerId: string, signInOptions?: any): Promise<SocialUser> {
+    return Promise.resolve(undefined);
+  },
+  signOut(revoke?: boolean): Promise<any> {
+    return Promise.resolve(undefined);
+  }
+};
+
+const MockRouter = {
+  parseUrl(url: string): UrlTree {
+    return new UrlTree();
+  },
+};
 describe('AuthGuard', () => {
   let guard: AuthGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [{provide: Router, useValue: MockRouter}, { provide: SocialAuthService, useValue: MockSocialAuthService}]
+    });
     guard = TestBed.inject(AuthGuard);
   });
 
