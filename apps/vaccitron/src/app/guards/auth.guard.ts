929247346df5b85470cd994cd6fe4d45910f850f
yonatankra <yonatan.kra@vonage.com>
2021-01-14 14:52:28 +0200

    Disable the auth guard until we get an approval from facebook

--- apps/vaccitron/src/app/guards/auth.guard.ts
+++ apps/vaccitron/src/app/guards/auth.guard.ts
@@ -14,6 +14,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
     return this.authService.authState.pipe(
       map(user => !user ?
         (state.url === '/login-page' ? true : this.router.parseUrl('/login-page')) :
