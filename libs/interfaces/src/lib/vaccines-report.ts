b5d0ae41bb881402dc4cedc3ea834048f94ac7d2
ErezEfrat <ereze2@gmail.com>
2021-01-17 15:17:04 +0200

    added endtime and minage to table

--- libs/interfaces/src/lib/vaccines-report.ts
+++ libs/interfaces/src/lib/vaccines-report.ts
@@ -1,16 +1,12 @@
 import { CITIES_TYPE, HEALTH_CARE_SERVICES_TYPE } from '@vacgaps/constants';
 
-interface Time {
-  hour: number;
-  minutes: number;
-}
 export interface VaccinesReport {
   branchName: string;
   city: CITIES_TYPE[number];
   address: string;
   healthCareService: HEALTH_CARE_SERVICES_TYPE[number];
-  startTime?: Time;
-  endTime?: Time;
+  startTime?: string;
+  endTime?: string;
   targetGroup?: number;
   minimalAge?: number;
   updateTime: number;
--- libs/reports-list/src/lib/reports-list/reports-list.component.ts
+++ libs/reports-list/src/lib/reports-list/reports-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { VaccinesReport } from '@vacgaps/interfaces';
+import { registerLocaleData } from '@angular/common';
+import localeIL from '@angular/common/locales/en-IL';
+registerLocaleData(localeIL, 'il');
 
 @Component({
   selector: 'vacgaps-reports-list',
