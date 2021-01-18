3958506658c103c8a26b8271358acc2b35d9f414
idigra <idigra@microsoft.com>
2021-01-18 11:32:50 +0200

    Fix tests

--- apps/vaccitron/src/app/report-list-page/report-list-page.component.spec.ts
+++ apps/vaccitron/src/app/report-list-page/report-list-page.component.spec.ts
@@ -107,6 +107,7 @@ describe('ReportListPageComponent', () => {
     it(`should update the filter when the filter fields change`, function() {
       const notificationsFilter: NotificationsFilter = {
         cities: ["100", "110"],
+        districts: [],
         healthCareService: "1",
         availableVaccines: 50,
         dueTimeInMs: 500,
@@ -127,6 +128,7 @@ describe('ReportListPageComponent', () => {
       component.reportsList = MOCK_REPORTS;
       const notificationsFilter: NotificationsFilter = {
         cities: ["100", "110"],
+        districts: [],
         healthCareService: "1"
       };
       component.updateFilter(notificationsFilter);
@@ -154,6 +156,7 @@ describe('ReportListPageComponent', () => {
       component.reportsList = MOCK_REPORTS.map(value => { value.healthCareService = "1"; return value; });
       const notificationsFilter: NotificationsFilter = {
         cities: [],
+        districts: [],
         healthCareService: "1"
       };
       component.updateFilter(notificationsFilter);
