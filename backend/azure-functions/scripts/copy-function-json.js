33d158a80bd42c1fed67566df89fdd45f94b9fc9
gilm123 <gilm123@gmail.com>
2021-01-21 20:03:17 +0200

    fixes

--- backend/azure-functions/scripts/copy-function-json.js
+++ backend/azure-functions/scripts/copy-function-json.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const glob = require('glob');
 const path = require('path');
 
-glob('src/*/function.json', (_err, result) => {
+glob('src/*/function.json', (err, result) => {
+    if (err) throw err;
     result.forEach(functionJsonFile => {
         const destinationFile = functionJsonFile.replace('src', 'dist');
         fs.mkdirSync(path.dirname(destinationFile));
