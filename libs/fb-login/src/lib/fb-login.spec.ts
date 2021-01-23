e3beb8b5433b7b71dd9a03b97e7e6d001ee90a40
yonatankra <yonatan.kra@vonage.com>
2021-01-23 20:23:03 +0200

    FbLogin Service

--- libs/fb-login/src/lib/fb-login.spec.ts
+++ libs/fb-login/src/lib/fb-login.spec.ts
@@ -1,7 +1,90 @@
-import { fbLogin } from './fb-login';
+import { FbLogin, UserDetails } from './fb-login';
+import { fbInitializer } from './fb-init/fb-initializer';
 
+const MOCK_FB_AUTH_DETAILS = {
+  accessToken: 'fake-access-token',
+  userID: 'fake-fb-id',
+  expiresIn: 5000
+};
+
+const EXPECTED_USER_DETAILS: UserDetails = {
+  extraInfo: '',
+  facebookId: MOCK_FB_AUTH_DETAILS.userID,
+  id: '',
+  name: '',
+  token: MOCK_FB_AUTH_DETAILS.accessToken,
+  expiresIn: MOCK_FB_AUTH_DETAILS.expiresIn
+};
+
+jest.mock('./fb-init/fb-initializer', () => (
+  {
+    fbInitializer: jest.fn((id) => new Promise(res => setTimeout(() => res(MOCK_FB_AUTH_DETAILS), 0)))
+  }
+));
+
+jest.useFakeTimers();
 describe('fbLogin', () => {
+  const config = {
+    fbAppId: 'fake-id'
+  };
+
+  let fbLogin: FbLogin;
+  beforeEach(function() {
+    fbLogin = new FbLogin(config);
+    window['FB'] = {
+      login: jest.fn(function (fn) {
+        fn({authResponse: MOCK_FB_AUTH_DETAILS});
+      }),
+    } as any;
+  });
+
   it('should work', () => {
-    expect(fbLogin()).toEqual('fb-login');
+    expect(FbLogin).toBeTruthy();
+  });
+
+  describe(`login`, function() {
+    it(`should return a promise that resolves after FB login with accessToken`, async function() {
+      const loginPromise = fbLogin.login();
+      expect(await loginPromise).toEqual(EXPECTED_USER_DETAILS);
+    });
+
+    it(`should return a promise that resolves after FB login with null if login failed`, async function() {
+      window['FB'] = {
+        login: jest.fn(function (fn) {
+          fn({authResponse: null});
+        }),
+      } as any;
+      const loginPromise = fbLogin.login();
+      expect(await loginPromise).toEqual(null);
+    });
+
+    it(`should set userDetails `, async function() {
+      await fbLogin.login();
+      expect(fbLogin.userDetails).toEqual(EXPECTED_USER_DETAILS);
+    });
+  });
+
+  describe(`init`, function() {
+    it(`should initialize the FB sdk`, function() {
+      expect(fbInitializer).toHaveBeenCalledWith(config.fbAppId);
+    });
+
+    it(`should set initState to true after initiatlization`, async function() {
+      jest.runAllTimers();
+      expect(await fbLogin.initState).toEqual(true);
+    });
+
+    it(`should set userDetails on successful login`, async function() {
+      jest.runAllTimers();
+      await fbLogin.initState;
+      expect(fbLogin.userDetails.token).toEqual(MOCK_FB_AUTH_DETAILS.accessToken);
+    });
+  });
+
+  it(`should set isLoggedIn according to userDetails`, function() {
+    fbLogin.userDetails = null;
+    expect(fbLogin.isLoggedIn).toEqual(false);
+    fbLogin.userDetails = EXPECTED_USER_DETAILS;
+    expect(fbLogin.isLoggedIn).toEqual(true);
   });
 });
--- libs/fb-login/src/lib/fb-login.ts
+++ libs/fb-login/src/lib/fb-login.ts
@@ -1,3 +1,46 @@
-export function fbLogin(): string {
-  return 'fb-login';
+import { fbInitializer } from './fb-init/fb-initializer';
+import AuthResponse = facebook.AuthResponse;
+import StatusResponse = facebook.StatusResponse;
+
+export interface FbLoginConfig {
+  fbAppId: string;
+}
+
+export interface UserDetails {
+  id: string;
+  facebookId: string;
+  name: string;
+  extraInfo: string;
+  token?: string;
+  expiresIn?: number;
+}
+
+export class FbLogin {
+  initState: Promise<boolean>;
+  userDetails: UserDetails;
+  get isLoggedIn(): boolean {
+    return !!this.userDetails;
+  }
+
+  constructor(private config: FbLoginConfig) {
+    this.initState = fbInitializer(config.fbAppId)
+      .then((authResponse: AuthResponse) => {
+        if (!this.userDetails) this.userDetails = { extraInfo: '', facebookId: '', id: '', name: '' };
+        this.userDetails.token = authResponse ? authResponse.accessToken : null;
+        return true;
+      });
+  }
+
+  login(): Promise<UserDetails> {
+    return new Promise((resolve) => {
+      FB.login((response: StatusResponse) => {
+        this.userDetails =  response.authResponse ? statusResponseToUserDetails(response) : null;
+        resolve(this.userDetails);
+      });
+    });
+  }
+}
+
+function statusResponseToUserDetails({authResponse}: StatusResponse): UserDetails {
+  return {token: authResponse.accessToken, extraInfo: '', facebookId: authResponse.userID, id: '', name: '', expiresIn: authResponse.expiresIn }
 }
