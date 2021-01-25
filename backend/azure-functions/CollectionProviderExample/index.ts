b6c048fe306015a8aa18f6324aeb502897e3309f
idigra <idigra@microsoft.com>
2021-01-25 12:29:31 +0200

    Revert "revert the prettier changes to BE"
    This reverts commit 367ad36fee25d6a7962c1b7afdc7dfa9aa1b6a4b.

--- backend/azure-functions/CollectionProviderExample/index.ts
+++ backend/azure-functions/CollectionProviderExample/index.ts
@@ -1,10 +1,13 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions";
-import { BaseEntity } from "../Models/BaseEntity";
-import { CollectionProvider } from "../Services/CollectionProvider"
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import { BaseEntity } from '../Models/BaseEntity';
+import { CollectionProvider } from '../Services/CollectionProvider';
 
-type ExampleEntity = BaseEntity & { foo: string; };
+type ExampleEntity = BaseEntity & { foo: string };
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {  
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
   // Collection Provider usage example
 
   const collection = await CollectionProvider.get<ExampleEntity>('example');
@@ -14,6 +17,6 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     status: 200,
     body: 'Hello from Collection Provider Example',
   };
-}
+};
 
 export default httpTrigger;
--- backend/azure-functions/Services/CollectionProvider.ts
+++ backend/azure-functions/Services/CollectionProvider.ts
@@ -1,37 +1,51 @@
-import { Db, MongoClient } from "mongodb";
-import { BaseEntity } from "../Models/BaseEntity";
-import { EnvironmentSettings } from "../Models/EnvironmentSettings";
-import { ICollection, patchMongoCollection } from "./ICollection";
+import { Db, MongoClient } from 'mongodb';
+import { BaseEntity } from '../Models/BaseEntity';
+import { EnvironmentSettings } from '../Models/EnvironmentSettings';
+import { ICollection, patchMongoCollection } from './ICollection';
 
 export class CollectionProvider {
   private static mongoDb: Promise<Db>;
-  private static collections: { [collection: string]: ICollection<any> }
+  private static collections: { [collection: string]: ICollection<any> };
   private static settings = new EnvironmentSettings();
-  
+
   private constructor() {}
 
-  public static async get<T extends BaseEntity>(collectionName: string): Promise<ICollection<T>> {
+  public static async get<T extends BaseEntity>(
+    collectionName: string
+  ): Promise<ICollection<T>> {
     if (!CollectionProvider.collections[collectionName]) {
-      CollectionProvider.collections[collectionName] = await CollectionProvider.createCollection<T>(collectionName);
+      CollectionProvider.collections[
+        collectionName
+      ] = await CollectionProvider.createCollection<T>(collectionName);
     }
 
     return CollectionProvider.collections[collectionName];
   }
 
-  private static async createCollection<T extends BaseEntity>(collectionName: string): Promise<ICollection<T>> {
+  private static async createCollection<T extends BaseEntity>(
+    collectionName: string
+  ): Promise<ICollection<T>> {
     if (CollectionProvider.mongoDb == null) {
       CollectionProvider.mongoDb = CollectionProvider.createMongoDb();
     }
-    const mongoCollection = patchMongoCollection<T>((await CollectionProvider.mongoDb).collection(collectionName));
+    const mongoCollection = patchMongoCollection<T>(
+      (await CollectionProvider.mongoDb).collection(collectionName)
+    );
 
     return mongoCollection;
   }
 
   private static async createMongoDb(): Promise<Db> {
     // connect and select database
-    const mongoClient = await MongoClient.connect(CollectionProvider.settings.mongoConnectionString,
-      { useUnifiedTopology: true, useNewUrlParser: true, tlsAllowInvalidCertificates: this.settings.allowSelfSignedMongoCert });
-    
+    const mongoClient = await MongoClient.connect(
+      CollectionProvider.settings.mongoConnectionString,
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        tlsAllowInvalidCertificates: this.settings.allowSelfSignedMongoCert,
+      }
+    );
+
     return mongoClient.db(this.settings.vacGapsDatabase);
   }
 }
