6af897217ca6fa64151fac65fe27febcd0aef653
idigra <idigra@microsoft.com>
2021-01-29 02:49:59 +0200

    Fixes for first prod deployment with BE

--- apps/vaccitron/src/environments/environment.ppe.ts
+++ apps/vaccitron/src/environments/environment.ppe.ts
@@ -2,9 +2,9 @@
 // Need to have also build mode for ppe environment, in which this file will be replaced.
 export const environment = {
   production: true,
-  vaccinesDataUrl: 'https://getvacci.azurewebsites.net/api/Reports',
+  vaccinesDataUrl: 'https://getvacci-ppe.azurewebsites.net/api/Reports',
   reportsQueryIntervalInMs: 5 * 60 * 1000,
   facebookAppId: '1279648522401260',
-  apiUrl: 'https://getvacci.azurewebsites.net/api/',
+  apiUrl: 'https://getvacci-ppe.azurewebsites.net/api/',
   comingFeedback: 'ComingFeedback',
 };
