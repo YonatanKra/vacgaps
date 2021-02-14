20e0d25d64ed025b56a759e77cdf947d9d16364d
idigra <42169023+idigra@users.noreply.github.com>
2021-02-14 20:44:35 +0200

    Vaccines updates client - reports to edit: filter hidden reports and add address in case of conflict (#86)

--- apps/vaccines-updates-client/src/components/main-form/parts/report-to-edit/index.tsx
+++ apps/vaccines-updates-client/src/components/main-form/parts/report-to-edit/index.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@material-ui/core';
+import { Button, Checkbox } from '@material-ui/core';
 import { CITIES } from '@vacgaps/constants';
 import { FormItem } from '../../form-item';
 import { NewReport, ReportIdOrNew, ReportOrNew, useFormData } from '../../../../providers/FormDataProvider';
@@ -6,8 +6,38 @@ import { useGetReports } from '../../../../hooks/useGetReports';
 import React, { FunctionComponent, useCallback, useEffect, useMemo } from 'react';
 import { VaccinesReport } from '@vacgaps/interfaces';
 import { AutoComplete, OptionType } from '../common/auto-complete';
+import styled from 'styled-components';
 
 type ReportOption = OptionType<ReportOrNew>;
+
+const ReportToEditColumnContainer = styled.div`   
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+`;
+
+const ReportToEditRowContainer = styled.div`   
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+
+    >*{
+        width: 100%;
+    }
+`;
+
+const ShowHiddenReportsContainer = styled.div`
+    display:flex;
+
+    margin-top: 10px;
+    &:first-child {
+        margin-top: 0;
+    }
+
+    label {
+        margin-right: 10px;
+    }
+`;
 
 const Comp: FunctionComponent<{ className?: string; }> = props => {
     const getReports = useGetReports(/*returnHiddenReports=*/true);
@@ -46,44 +76,79 @@ const Comp: FunctionComponent<{ className?: string; }> = props => {
         existingReport.targetGroups?.forEach(_ => formData.addTargetGroup(_));
     }, [formData, formData.setReportIdToEdit]);
 
-    const newReport: ReportOption = useMemo(() => ({
+    const newReportOption: ReportOption = useMemo(() => ({
         text: 'הוסף דיווח חדש',
         value: NewReport,
     }), []);
 
+    function groupBy<T>(array: T[], keySelector: (T) => string): { [key: string]: T[] } {
+        return array.reduce((accumulated, item) => {
+            const key: string = keySelector(item);
+            accumulated[key] = accumulated[key] || [];
+            accumulated[key].push(item);
+            return accumulated;
+        }, {});
+    }
+
     const reportOptions = useMemo(() => {
         const reports = formData.availableReportsToEdit?.reports;
-        if (!reports) return [newReport];
+        if (!reports) return [newReportOption];
 
-        const alreadySeenText: { [text: string]: boolean } = {};
-        return reports.map(report => {
-            if (report === NewReport) {
-                return newReport;
-            }
+        const filteredReports = reports.filter(report => formData.showHiddenReports || report === NewReport || !report.report.hideReport);
 
-            let text = CITIES[report.report.city].name;
-            let nextIndex = 1;
-            while (alreadySeenText[text]) {
-                text = CITIES[report.report.city].name + ' (' + (++nextIndex) + ')';
-            }
-
-            alreadySeenText[text] = true;
-
-            return {
-                text,
+        const groupedByCity: { [city: string]: VaccinesReport[] } = groupBy(
+            filteredReports.filter(report => report !== NewReport)
+                   .map(report => (report as { report: VaccinesReport }).report),
+            report => report.city);
+        
+        const groupedByCityAndAddress: [string, [string, VaccinesReport[]][]][] =
+            Object.entries(groupedByCity)
+                  .map(entry => [entry[0], Object.entries(groupBy(entry[1], report => report.address))]);
+        
+        const countsByCityAndAddress: { [city: string]: { count: number, byAddress: { [address: string]: { count: number, alreadyAdded: number } } } } =
+            Object.fromEntries(groupedByCityAndAddress.map(cityEntry =>
+                [cityEntry[0], {
+                    count: cityEntry[1].length,
+                    byAddress: Object.fromEntries(cityEntry[1].map(addressEntry =>
+                        [addressEntry[0], { count: addressEntry[1].length, alreadyAdded: 0 }]
+                    ))
+                }]
+            ));
+        
+        const options = filteredReports
+            .map(report => report === NewReport ? newReportOption : {
+                text:
+                    CITIES[report.report.city].name +
+                    (countsByCityAndAddress[report.report.city].count > 1 ? ' - ' + report.report.address : '') +
+                    (countsByCityAndAddress[report.report.city].byAddress[report.report.address].count > 1 ? ' (' + (countsByCityAndAddress[report.report.city].byAddress[report.report.address].alreadyAdded += 1) + ')' : ''),
                 value: report,
-            } as ReportOption;
         });
-    }, [formData, newReport]);
+
+        return options;
+    }, [formData, newReportOption]);
 
     return (
         <FormItem className={props.className}>
-            <h3>בחירת דיווח לעריכה:</h3>
-            <AutoComplete<ReportOrNew>
-                options={reportOptions}
-                onChange={value => setReportToEdit(value)}
-            />
-            <Button onClick={refreshReports}>רענן דיווחים</Button>
+            <ReportToEditColumnContainer>
+                <ReportToEditRowContainer>
+                    <h3>בחירת דיווח לעריכה:</h3>
+                    <AutoComplete<ReportOrNew>
+                        options={reportOptions}
+                        onChange={value => setReportToEdit(value)}
+                    />
+                    <Button onClick={refreshReports}>רענן דיווחים</Button>
+                </ReportToEditRowContainer>
+                <ReportToEditRowContainer>
+                    <ShowHiddenReportsContainer>
+                        <Checkbox
+                                id='showHiddenReportsCheck'
+                                onChange={_ => formData.setShowHiddenReports(_.target.checked)}
+                                checked={!!formData.showHiddenReports}
+                        />
+                        <label htmlFor='showHiddenReportsCheck'>הצג דיווחים מוסתרים</label>
+                    </ShowHiddenReportsContainer>
+                </ReportToEditRowContainer>
+            </ReportToEditColumnContainer>
         </FormItem>
     );
 };
